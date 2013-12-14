8675ac7a39e06a7e98ca086088e2a40256dc4590
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2013-12-14 16:59:55 +0100

    cleaned the dist.sh script thanks to ShellCheck

--- dist.sh
+++ dist.sh
@@ -2,17 +2,20 @@
 
 if [ $# != 1 ]
 then
-	echo -e "Usage:\n $0 <version>\n $0 <tree-ish>" >&2
+	cat >&2 <<-EOF
+	Usage:
+	  $0 <version>
+	  $0 <tree-ish>
+	EOF
 	exit 1
 fi
 
 label=libvmod-querystring-$1
 
-if [ `git tag -l v$1 | wc -l` = 1 ]
+if [ $(git tag -l "v$1" | wc -l) = 1 ]
 then
-	git archive --prefix $label/ -o v$1.tar.gz v$1
+	git archive --prefix "$label/" -o "v$1.tar.gz" "v$1"
 	exit
 fi
 
-git archive --prefix $label/ -o $label.tar.gz $1
-
+git archive --prefix "$label/" -o "$label.tar.gz" "$1"
