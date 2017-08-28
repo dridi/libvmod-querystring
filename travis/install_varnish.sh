b839e8a84e31556ab717c9366a1ae03bc01c4914
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-08-28 19:35:08 +0200

    Move the Varnish installation to a script
    This is for Travis CI and the new script makes it easier to support cron
    jobs. While at it, update the download URL with the approaching sunset
    of repo.varnish-cache.org for releases and packages.

---
+++ travis/install_varnish.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Written by Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
+#
+# This file is in the public domain.
+
+set -e
+set -u
+
+VERSION=5.1.1
+
+if [ "${TRAVIS_EVENT_TYPE:-}" = cron ]
+then
+	wget https://github.com/varnishcache/varnish-cache/archive/master.tar.gz
+	tar xf master.tar.gz
+	cd varnish-cache-master/
+	./autogen.sh
+else
+	wget https://varnish-cache.org/_downloads/varnish-$VERSION.tgz
+	tar xf varnish-$VERSION.tgz
+	cd varnish-$VERSION/
+fi
+
+./configure --prefix=/usr
+make -sj32
+sudo make install
