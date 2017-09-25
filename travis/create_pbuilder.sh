76d21d4770db6050ee36157943dc15a2d1dfef12
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2017-09-25 23:04:01 +0200

    Don't use pbuilder unless needed

---
+++ travis/create_pbuilder.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Written by Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
+#
+# This file is in the public domain.
+
+set -e
+set -u
+
+if [ "${MAKE_TARGET:-}" = pdebuild ]
+then
+	sudo pbuilder create \
+		--distribution sid \
+		--mirror ftp://ftp.us.debian.org/debian/ \
+		--debootstrapopts \
+		"--keyring=/usr/share/keyrings/debian-archive-keyring.gpg"
+fi
