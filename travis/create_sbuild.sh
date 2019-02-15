49863ce4ee1e38a039300b5f77a676f0fa211e7f
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2019-02-15 17:32:34 +0100

    Missing file

---
+++ travis/create_sbuild.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Written by Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
+#
+# This file is in the public domain.
+
+set -e
+set -u
+
+if [ "${MAKE_TARGET:-}" = sbuild ]
+then
+	sudo sbuild-createchroot sid --verbose \
+		/srv/chroot/sid-amd64-sbuild \
+		http://ftp.us.debian.org/debian/
+fi
