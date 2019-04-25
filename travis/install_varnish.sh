fb2f079128c8675ad470fcbc6819cda8362c8765
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2019-04-25 23:07:02 +0200

    Always build against supported Varnish branches
    Instead of only running master builds for the weekly cron job, do it all
    the time. The drawback is that only a couple jobs will run against the
    master branch instead of the complete matrix. But in addition, both
    normal and cron jobs will build against 6.0 LTS too.

--- travis/install_varnish.sh
+++ travis/install_varnish.sh
@@ -9,17 +9,25 @@ set -u
 
 VERSION=6.0.0
 
-if [ "${TRAVIS_EVENT_TYPE:-}" = cron ]
-then
+case ${VARNISH_BRANCH:-} in
+master)
 	wget https://github.com/varnishcache/varnish-cache/archive/master.tar.gz
 	tar xf master.tar.gz
 	cd varnish-cache-master/
 	./autogen.sh
-else
+	;;
+6.0)
+	wget https://github.com/varnishcache/varnish-cache/archive/6.0.tar.gz
+	tar xf 6.0.tar.gz
+	cd varnish-cache-6.0/
+	./autogen.sh
+	;;
+*)
 	wget https://varnish-cache.org/_downloads/varnish-$VERSION.tgz
 	tar xf varnish-$VERSION.tgz
 	cd varnish-$VERSION/
-fi
+	;;
+esac
 
 ./configure --prefix=/usr
 make -sj32
